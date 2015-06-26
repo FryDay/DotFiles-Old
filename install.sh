5e9dd88752f925a142ab49eef2a53e470ceecfee
Jordan Day <jordanday444@gmail.com>
2015-06-26 14:34:17 -0500

    Now using stow.

--- install.sh
+++ install.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
 
-########## Variables
-
-dir=~/.dotFiles                    # dotfiles directory
-olddir=~/.dotFiles_Old             # old dotfiles backup directory
-files="i3 vimrc"    # list of files/folders to symlink in homedir
-##########
-
-# create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-echo "done"
-
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
-echo "done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file $olddir
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
-done
+stow i3
+stow vim
+stow termite
